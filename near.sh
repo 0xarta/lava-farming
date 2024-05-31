d2125ae09c36c287bd73dde1b529cc9a631a9a73
0xarta <161648615+0xarta@users.noreply.github.com>
2024-05-31 22:37:29 +0200

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-48274809-b7b6-4920-9a1a-972a07c1e935/"
 
 OUTPUT_DIR="logs"
 
